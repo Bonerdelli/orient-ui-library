f67af8966777772cbcc2d3711203f47ca01fd8d4
Andrey Nekrasov <bonerdelli@gmail.com>
2022-04-28 15:32:14 +0300

    Move API wrappers and schema generation

---
+++ src/library/api/user.ts
@@ -0,0 +1,10 @@
+import { User, ApiErrorResponse /* , get */ } from 'library/helpers/api'
+
+import mockData from 'library/mock/user'
+
+export async function getCurrentUser(/* id: string */): Promise<
+User | ApiErrorResponse
+> {
+  // const result = await get<User | ApiErrorResponse>('user/current') // NOTE: sample API call
+  return mockData
+}
---
+++ src/library/mock/user.ts
@@ -0,0 +1,8 @@
+import { User } from 'orient-ui-library'
+
+const mockData: User = {
+  login: 'admin',
+  fullName: 'Administrator',
+}
+
+export default mockData
